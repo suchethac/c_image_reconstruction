5eac2d16442581f0b0347a6b94e3d7584fe19bd9
suchethac <cooray@nagoya-u.jp>
2018-09-09 19:25:00 +0900

    First completely working notebook with module

--- img_recon.py
+++ img_recon.py
@@ -11,8 +11,9 @@ from astropy.io import fits
 import os, sys
 
 class Image():
-    def __init__(self, image, fits_header=None):
-        self.data = image
+    def __init__(self, data, fits_header=None):
+        self.data = data
+        self.fits_header = fits_header
 
     def from_file(filename, nanzero=True):
         cube=fits.open(filename)#data_dir+cube_name)
@@ -24,6 +25,8 @@ class Image():
                     pass
                 else:
                     img[i,j]=0
+
+        img = img.astype(float)
         return Image(img,fits_header=cube[0].header)
 
     def recon_img(image, window, smoothed_window, f_window):
@@ -53,26 +56,69 @@ class Image():
         return o_a
 
     def f_window(img, freq_cut):
-        shp_x, shp_y = img.shape
-        f_wind = np.ones(shp_x, shp_y)
+        img_shp = (0,0)
+        if isinstance(img, (Image, ImageObj)):
+            img_shp = img.data.shape
+        elif isinstance(img,(np.ndarray, np.generic)):
+            img_shp = img.shape
+        shp_x, shp_y = int((img_shp[0]+1)/2), int((img_shp[1]+1)/2)
+        f_wind = np.ones(img_shp)
         for (y,x), value in np.ndenumerate(f_wind):
             if (x-shp_x+1)**2+(y-shp_y+1)**2>freq_cut**2:
                 f_wind[y,x]=0
-        return f_wind
+        return f_wind.astype(float)
 
-    def reconstruct(img, msk, freq_cut=None):
-        if not isinstance(img, Image):
-            return print("invalid image object type")
+    def reconstruct_image(img, msk, freq_cut):
+        # if not isinstance(img, Image):
+        #     return print("invalid image object type")
         if not isinstance(msk, (Mask, MaskObj)):
             return print("invalid mask object type")
         if freq_cut is None: freq_cut=img.shape[0]/2
 
-        s_wind = smooth_boundary(msk.window)
-        f_wind = f_window(img.data, freq_cut)
-        reconstructed_img = recon_img(img.data, msk.window, s_wind, f_wind)
+        s_wind = Mask.smooth_boundary(msk.window)
+        f_wind = Image.f_window(img.data, freq_cut)
+        reconstructed_img = Image.recon_img(img.data, msk.window, s_wind, f_wind)
 
         return ImageObj(reconstructed_img, msk, reconstructed=True, \
                     freq_cut=freq_cut, fits_header=img.fits_header)
+
+    def reconstruct(img, msk=None, freq_cut=None):
+        """
+        Wrapper to deal with many data types
+        """
+        if not isinstance(img, ImageObj) and (msk is None or freq_cut is None):
+            return print("Insufficient information for reconstruction")
+
+        if isinstance(img, ImageObj):
+            image_data = img
+            mask_data = img.mask
+            freq_cut = img.freq_cut
+
+        if isinstance(img, Image) and msk is not None:
+            image_data = img
+            mask_data = msk
+            if freq_cut is None: freq_cut=img.shape[0]/2
+            freq_cut = freq_cut
+
+        ## include cases where the oimage and the mask are arrays
+        return Image.reconstruct_image(image_data, mask_data, freq_cut)
+
+    def f_image(img):
+        """
+        Outputs the Fourier transform of the image as an array
+        """
+        arr = img
+
+        if isinstance(img, (Image, ImageObj)):
+            arr = img.data
+        elif isinstance(img,(np.ndarray, np.generic)):
+            arr = img
+
+        arr = arr.astype(float)
+        f_est = fftpack.fft2(arr)
+        f_est = fftpack.fftshift(f_est)
+
+        return f_est.astype(float)
 
     # def _reconstruct(img_obj):#Not used
     #     if not isinstance(img_obj, Image):
@@ -112,7 +158,10 @@ class ImageObj(Image):
     def __init__(self, image, mask, reconstructed=False, \
                 freq_cut=0, fits_header=None):
 
-        super().__init__(image, fits_header=None)
+        if isinstance(image, (Image, ImageObj)):
+            super().__init__(image.data, fits_header=image.fits_header)
+        elif isinstance(image,(np.ndarray, np.generic)):
+            super().__init__(image, fits_header=None)
 
         if isinstance(mask, (Mask, MaskObj)):
             self.mask = mask
@@ -146,8 +195,9 @@ class Mask():
         mask -= window
         return mask
 
-    def smooth_boundary(i_a, sigma=2):
-        shp_x, shp_y = i_a.shape
+    def smooth_boundary(i_a, sigma=3):
+        img_shp = i_a.shape
+        shp_x, shp_y = int((img_shp[0]+1)/2), int((img_shp[1]+1)/2)
         x, y = np.meshgrid(np.linspace(-shp_x,shp_x,2*shp_x), \
                             np.linspace(-shp_y,shp_y,2*shp_y))
         sigma_x, sigma_y = sigma, sigma
@@ -157,13 +207,22 @@ class Mask():
         #Generating a smoothed window
         o_a=np.copy(i_a)
         i=0
-        while i<5:
+        while i<10:
             o_a = ndimage.convolve((o_a*i_a), gauss, mode='nearest')
             i+=1
         return o_a
 
-    def error_def_line(img, hline=None, vline=None):
-        img_shp = img.shape
+    def err_def_line(img, hline=None, vline=None):
+        """
+        Define a error array from row or column number
+        vlines showing as hlines
+        """
+        img_shp = (0,0)
+        if isinstance(img, (Image, ImageObj)):
+            img_shp = img.data.shape
+        elif isinstance(img,(np.ndarray, np.generic)):
+            img_shp = img.shape
+
         error_array = np.ones(img_shp)
 
         if hline is not None:
@@ -171,20 +230,20 @@ class Mask():
                 hline_min = hline - 1
                 error_array[hline_min:hline][:]=0
             elif isinstance(hline, list):
-                hline_min = hline - 1
+                hline_min = [x - 1 for x in hline]
                 for a,b in zip(hline_min, hline):
-                    error_array[hline_min:hline][:]=0
+                    error_array[a:b][:]=0
 
         if vline is not None:
             if isinstance(vline, int):
                 vline_min = vline - 1
-                error_array[:][vline_min:vline]=0
+                error_array[0:img_shp[0]][vline_min:vline]=0
             elif isinstance(vline, list):
-                vline_min = vline - 1
+                vline_min = [y - 1 for y in vline]
                 for a,b in zip(vline_min, vline):
-                    error_array[:][vline_min:vline]=0
+                    error_array[0:img_shp[0]][a:b]=0
 
-        return error_array
+        return Mask(error_array)
 
 
     def error_line(mask_obj, err_line, elwidth = 1):
